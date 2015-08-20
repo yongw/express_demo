efa554ab79169938f78c853acf81455eb7662577
root <root@k8s-minion-node-1-a60b9d56-6ed2-441c-8a4d-27316c9be960.amers2.ci>
2015-08-20 09:48:40 +0000

    fix minor issues

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log('Accessing home...');
   var connection = mysql.createConnection({
     host: process.env.MYSQL_PORT_3306_TCP_ADDR,
     user: 'root',
@@ -13,14 +14,17 @@ router.get('/', function (req, res, next) {
   });
 
   connection.connect();
-
+  
+  console.log('Connected to Mysql' + process.env.MYSQL_PORT_3306_TCP);
   connection.query('SELECT * FROM user', function (err, rows, fields) {
     if (err) throw err;
-
+    
+    console.log('Get data from my-sql:' + rows);
     res.render('index', { title: 'My Web Application', users: rows});
   });
 
   connection.end();
+  console.log('Leaving home...');
 });
 
 module.exports = router;
